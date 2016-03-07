7a7400a25350f734047cc457a9e43d320b929a69
sestens <santiago.estens@gmail.com>
2016-03-07 01:51:44 -0600

    Coded demo logins and a main menu layout.

---
+++ app/src/main/java/com/example/sestens/pocket_advisor/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.sestens.pocket_advisor;
+
+import android.os.Bundle;
+
+/**
+ * Created by sestens on 3/7/2016.
+ */
+public class MainActivity extends android.app.Activity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);             //Display Main Menu (Degree Plans)
+    }
+
+
+}
+
