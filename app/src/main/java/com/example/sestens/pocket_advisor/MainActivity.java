f876728d36d86db97456fabbc499f2fa4d362838
Mark Nissen <marknissen11@gmail.com>
2016-04-21 19:44:04 -0500

    Added menu with "GPA Calculator" and "Log Out" options to the Degree Plan selection screen. Currently just display toast message.

--- app/src/main/java/com/example/sestens/pocket_advisor/MainActivity.java
+++ app/src/main/java/com/example/sestens/pocket_advisor/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.sestens.pocket_advisor;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 /**
  * Created by sestens on 3/7/2016.
@@ -13,5 +20,36 @@ public class MainActivity extends android.app.Activity {
     }
 
 
+    public void showPopUp(View view){
+        PopupMenu popupMenu = new PopupMenu(this,view);
+        MenuInflater menuInflater = popupMenu.getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, popupMenu.getMenu());
+        popupMenu.show();
+        popupMenu.setOnMenuItemClickListener(
+                new PopupMenu.OnMenuItemClickListener(){
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item){
+                        if(item.getItemId() == R.id.gpa_calculator){
+                            Toast.makeText(MainActivity.this, "GPA Calc",Toast.LENGTH_LONG).show();
+                            //Intent intent = new Intent("com.example.test.GPACalculator");
+                            //startActivity(intent);
+                        }
+                        if(item.getItemId() == R.id.log_out){
+                            Toast.makeText(MainActivity.this, "Logging out",Toast.LENGTH_LONG).show();
+                        }
+                        return false;
+                    }
+                }
+        );
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
 }
 
