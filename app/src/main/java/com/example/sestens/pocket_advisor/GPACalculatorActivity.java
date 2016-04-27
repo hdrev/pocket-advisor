e62157cf60488842e6ef6d88b09578f3b60528aa
Mark Nissen <marknissen11@gmail.com>
2016-04-26 19:12:16 -0500

    updated content_gpacalculator.xml and started functionality for gpa calculator.

--- app/src/main/java/com/example/sestens/pocket_advisor/GPACalculatorActivity.java
+++ app/src/main/java/com/example/sestens/pocket_advisor/GPACalculatorActivity.java
@@ -2,6 +2,9 @@ package com.example.sestens.pocket_advisor;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class GPACalculatorActivity extends AppCompatActivity {
@@ -10,8 +13,25 @@ public class GPACalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gpacalculator);
-        //GPACalculator calc = new GPACalculator();
+        GPACalculator calc = new GPACalculator();
+
+        //int totalHours = calc.getTotalHours();
+        final TextView current_hours = (TextView) findViewById(R.id.currentHours);
+
+        current_hours.setText("");
+        Button addCourseButton = (Button) findViewById(R.id.addCourse);
+
+       addCourseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //current_hours.setText("");
+
+           }
+        });
+
 
     }
+
+
 
 }
